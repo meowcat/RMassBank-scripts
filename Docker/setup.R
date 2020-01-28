8dd29cd038745e75d41eef53051803f4da54bd29
meowcat <stravsmi at eawag.ch>
2020-01-28 19:33:42 +0100

    Dockerfile

---
+++ Docker/setup.R
@@ -0,0 +1,9 @@
+
+install.packages("devtools")
+
+install.packages("tidyverse")
+install.packages("drake")
+
+library("devtools")
+install_github("MassBank/RMassBank@s4power")
+install_github("dutchjes/MSMSsim")
