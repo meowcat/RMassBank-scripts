b43d30e3c1b627b6f08e71bccf079d69ab491045
Michele Stravs <stravsmi@eawag.ch>
2022-06-16 16:35:12 +0200

    backported mbWorkflow improvements from NZ workflow

--- scripts/07_process_mbworkflow.R
+++ scripts/07_process_mbworkflow.R
@@ -2,43 +2,70 @@ library(here)
 
 library(RMassBank)
 library(tidyverse)
+source(here("functions.R"))
 
 loadList("results/compoundsRmb.csv")
 loadRmbSettings("input/RmbSettings.ini")
 
 
-#w <- loadMsmsWorkspace("results/spectra-pH-processed.RData")
 
-load("results/review-pH.RData")
+infolists <- c()
 
-# adjust the data in the spectra as long as there is no other way to select it
-w_ <- review$w
-for(i in seq_along(w_@spectra))
-  attr(w_@spectra[[i]], "specOK") <- review$specOk[[i]]
-w_@spectra <- w_@spectra[review$cpdOk]
+charge_strs <- c("pH", "mH")
 
-metric <- "eicScoreCor"
-eicScoreLimit <- attr(w_, "eicScoreFilter")[[metric]]
-
-w_@spectra <- as(lapply(w_@spectra, function(cpd) {
-  specOK <- attr(w_, "specOK")
-  for(i in seq_along(specOK))
-    cpd@children[[i]]@ok <- specOK[[i]]
-  cpd@children <- as(lapply(cpd@children, function(sp) {
-    d <- getData(sp)
-    d <- d %>% group_by(mz) %>% arrange(!good, abs(dppm)) %>% slice(1)
-    d <- d %>% mutate(
-      good_ = good,
-      good = eicScoreCor > eicScoreLimit
-    ) %>% filter(good)
-    sp <- setData(sp, as.data.frame(d))
-    sp
+walk(charge_strs, function(charge_str) {
+  
+  
+  cpdOk <- read_csv(glue("input/review_{charge_str}_cpd_ok.csv")) %>% pull(ok)
+  specOk <- read_csv(glue("input/review_{charge_str}_spec_ok.csv")) %>%
+    group_by(cpd) %>%
+    group_split() %>%
+    map(~.x$ok)
+  
+  cutoff_file <- glue("input/review_{charge_str}_score_cutoff.csv")
+  if(fs::file_exists(cutoff_file))
+    score_cutoff <- read_file(cutoff_file) %>% as.numeric()
+  else
+    score_cutoff <- NA
+  
+  
+  w_ <- loadMsmsWorkspace(glue("results/spectra-{charge_str}-autoreview.RData"))
+  
+  # Select and deselect spectra based on the choices from the review files
+  for(i in seq_along(w_@spectra))
+    attr(w_@spectra[[i]], "specOK") <- specOk[[i]]
+  w_@spectra <- w_@spectra[cpdOk]
+  
+  metric <- "eicScoreCor"
+  if(!is.na(score_cutoff))
+    eicScoreLimit <- score_cutoff
+  else
+    eicScoreLimit <- attr(w_, "eicScoreFilter")[[metric]]
+  
+  w_@spectra <- as(lapply(w_@spectra, function(cpd) {
+    specOK <- attr(w_, "specOK")
+    for(i in seq_along(specOK))
+      cpd@children[[i]]@ok <- specOK[[i]]
+    cpd@children <- as(lapply(cpd@children, function(sp) {
+      d <- getData(sp)
+      d <- d %>% group_by(mz) %>% arrange(!good, abs(dppm)) %>% slice(1)
+      d <- d %>% mutate(
+        good_ = good,
+        good = eicScoreCor > eicScoreLimit
+      ) %>% filter(good)
+      sp <- setData(sp, as.data.frame(d))
+      sp
+    }), "SimpleList")
+    cpd
   }), "SimpleList")
-  cpd
-}), "SimpleList")
-
-mb <- newMbWorkspace(w_)
-
-mb <- mbWorkflow(mb, steps=c(1:3), infolist_path = "infolist_pH.csv")
-save(mb, file="results/records_pH.RData")
-
+  
+  mb <- newMbWorkspace(w_)
+  for(infolist in infolists)
+    mb <- loadInfolist(mb, infolist)
+  newInfolist <- glue("results/infolist_{charge_str}.csv")
+  mb <- mbWorkflow(mb, steps=c(1:3), 
+                   infolist_path = newInfolist)
+  
+  infolists <<- c(infolists, newInfolist)
+  save(mb, file=glue("results/records_{charge_str}.RData"))
+})
