70b132977f9a5da6a45ea246d89306a47ab2da16
Michele Stravs <stravsmi@eawag.ch>
2023-03-14 14:24:10 +0100

    environment for multiplexing

--- environment.R
+++ environment.R
@@ -1,2 +1,4 @@
-inputDir <- c("C:\\Daten\\Michele\\20190625 Karin MassBank-QE\\mzXML")
-compoundsCsv <- "input/SubstanzenKarin_Massbank.csv"
+inputDir <- c("C:\\Daten\\RMassBank\\birgit_data\\mzml")
+inputMultiplex <- c("C:\\Daten\\RMassBank\\birgit_data\\mzml_multi")
+compoundsCsv <- "input/massbank.csv"
+column_of_id <- 1
